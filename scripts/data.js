360a7f9c57829dd8d59ebe01923dad76dd82a11d
Mitch McCullough <mitchmccullough@gmail.com>
2022-04-30 12:32:36 -0500

    added files, initial setup code

---
+++ scripts/data.js
@@ -0,0 +1,54 @@
+const students = [
+  {
+    id: 0,
+    name: "Bill Haverford",
+    house: '',
+  },
+  {
+    id: 1,
+    name: "Sarah Hensgrove",
+    house: '',
+  },
+  {
+    id: 2,
+    name: "Adam Buchemal",
+    house: '',
+  },
+  {
+    id: 3,
+    name: "Dee Haverford",
+    house: '',
+  },
+  {
+    id: 4,
+    name: "Sebastion Toilet jr.",
+    house: '',
+  },
+  {
+    id: 5,
+    name: "Lenny Spruce",
+    house: '',
+  },
+  {
+    id: 6,
+    name: "Tammy Turnover",
+    house: '',
+  },
+  {
+    id: 7,
+    name: "Kristi Kreme",
+    house: '',
+  },
+  {
+    id: 8,
+    name: "Drill Noises, M.d",
+    house: '',
+  },
+  {
+    id: 9,
+    name: "AHHHHHHHHHH, jr.",
+    house: '',
+  },
+];
+
+export default students;
